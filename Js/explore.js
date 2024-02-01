6f4aa26b7353a435ce4cc2f9041e64b3ebee2a4a
satyam <livester044@gmail.com>
2024-02-01 14:53:07 +0530

    explore sec chsnges

--- Js/explore.js
+++ Js/explore.js
@@ -1,4 +1,42 @@
 function ExSearch() {
+    let id = 0;
+
+    const arr = [
+        "model",
+        "fashion",
+        "style",
+        "beauty",
+        "glamour",
+        "girl",
+        "posing",
+        "trend",
+        "vogue",
+        "high fashion",
+        "luxury",
+        "lifestyle",
+        "urban",
+        "trendy",
+        "photography",
+        "beautiful",
+        "photooftheday",
+        "travel",
+        "adventure",
+        "explore",
+        "instagood",
+        "instastyle",
+        "fashionista",
+        "vibes",
+        "confidence",
+        "swagger",
+        "news",
+        "chic",
+        "modern",
+        "classy",
+        "edgy",
+        "trendsetter",
+        "grace",
+    ];
+
     const accessKey = "42130655-4dc8270fabe9e178cce07c32c";
 
     let formSubmit = document.querySelector("#Ex-search")
@@ -14,26 +52,25 @@ function ExSearch() {
 
         const response = await fetch(searchUrl);
         const data = await response.json();
-
+                
         ExHtml = `
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
-        <img src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+        <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+        <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + keyword}" alt="">
+        <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+        <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+        <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+        <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+        <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+        <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+        <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+        <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+        <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+        <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
         `
         searchResult.innerHTML += ExHtml;
     }
-    keydown = searchBar.addEventListener("keydown", function () {
+    keydown = searchBar.addEventListener("keypress", function () {
         searchResult.innerHTML = "";
-        getImage()
     })
 
     formSubmit.addEventListener("submit", (e) => {
@@ -46,15 +83,60 @@ function ExSearch() {
     });
     
     window.addEventListener("load",()=>{
+        async function getImage() {
+            keyword = searchBar.value;
+            const searchUrl = `https://pixabay.com/api/?key=${accessKey}&q=${keyword}&pretty=true`;
+    
+            const response = await fetch(searchUrl);
+            const data = await response.json();
+                    
+            ExHtml = `
+            <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+            <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + keyword}" alt="">
+            <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+            <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+            <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+            <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+            <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+            <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+            <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+            <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+            <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+            <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+            `
+            searchResult.innerHTML += ExHtml;
+        }
         getImage()
     })
-    window.addEventListener("scroll", function (event) {
+    window.addEventListener("scroll", function () {
         scrollable = document.documentElement.scrollHeight - window.innerHeight;
         scrollend = window.scrollY;
 
         if (Math.ceil(scrollend) === (scrollable)) {
+            async function getImage() {
+                keyword = searchBar.value;
+                const searchUrl = `https://pixabay.com/api/?key=${accessKey}&q=${keyword}&pretty=true`;
+        
+                const response = await fetch(searchUrl);
+                const data = await response.json();
+
+                ExHtml = `
+                <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+                <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+                <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+                <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+                <img id="${id+++1}" src="${data.hits[Math.floor(Math.random() * 20)].largeImageURL}" alt="">
+                <img id="${id+++1}" src="${"https://source.unsplash.com/random/?" + arr[Math.floor(Math.random() * arr.length)]}" alt="">
+                `
+                searchResult.innerHTML += ExHtml;
+            }
             getImage()
         }
+    })
+    
+    let filledImg = searchResult
+    filledImg.addEventListener("click",function(e){
+        console.log(e.target.id)
     })
 }
 
