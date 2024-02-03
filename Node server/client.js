a4bf557e17a6f75a1854f7718e61fb526ee610e1
satyam <livester044@gmail.com>
2024-02-03 21:52:04 +0530

    ui change msg

--- Node server/client.js
+++ Node server/client.js
@@ -1,4 +1,3 @@
-// import {io} from 'socket.io'
 const socket = io('http://localhost:3000');
 
 const form = document.getElementById("send-container");
@@ -6,12 +5,6 @@ const msgInp = document.getElementById("msgInp");
 const msgContLeft = document.querySelector(".msgContLeft");
 const msgCont = document.querySelector(".msgCont");
 
-const msgLDiv = (name)=>{
-    let msgDiv = document.createElement('div')
-    msgDiv.innerText = name;
-    msgDiv.classList.add("msgNames")
-    msgContLeft.append(msgDiv)
-}
 
 const append = (message, position) => {
     const msgElem = document.createElement('div');
@@ -29,16 +22,39 @@ form.addEventListener("submit", (e) => {
     msgInp.value = "";
 })
 
-const name = prompt("Enter Your Name");
-socket.emit('new-user-joined', name);
+let name;
+
+do {
+    name = prompt("Enter Your Name");
+    socket.emit('new-user-joined', name);
+} while (name === "");
 
 socket.on('user-joined', name => {
-    msgLDiv(`${name}`)
-    append(`${name} joined the chat`, 'left')
+    if (name != "" && name != null) {
+        const msgLDiv = (name) => {
+            let msgDiv = document.createElement('div')
+            msgDiv.innerText = name;
+            msgDiv.classList.add("msgNames")
+            msgContLeft.append(msgDiv)
+        }
+        msgLDiv(`${name}`)
+        append(`${name} joined the chat`, 'left')   
+    }
 })
 socket.on('receive', data => {
     append(`${data.name}:${data.message}`, 'left')
 })
 socket.on('left', name => {
-    append(`${name} left the chat`, 'left')
+    if (name != null) {
+    const msgLDiv = (name) => {
+        let msgDiv = document.createElement('div')
+        msgDiv.innerText = name;
+        msgDiv.classList.add("msgNames")
+        msgDiv.style.backgroundColor = "red"
+        msgDiv.style.color = "white"
+        msgContLeft.append(msgDiv)
+    }
+    msgLDiv(`${name} Left`)
+    append(`${name}: left the chat`, 'left')   
+    }
 })
